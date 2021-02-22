1c697267d23b6d9d6d39d2e58d36988e738774d2
ilya-brinchuk <ilya.brinchuk@gmail.com>
2021-02-22 17:47:19 +0200

    added Logger

--- src/main/java/MyThread.java
+++ src/main/java/MyThread.java
@@ -1,9 +1,11 @@
+import org.apache.log4j.Logger;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
 public class MyThread extends Thread {
+    private static final Logger logger = Logger.getLogger(MyThread.class);
     private static final String PATH_FILE = "src/main/resources/test.txt";
     private Counter counter;
 
@@ -15,9 +17,10 @@ public class MyThread extends Thread {
     public void run() {
         while (counter.getCount() < 100) {
             try {
-                String data = Thread.currentThread().getName() + " " + counter.getCount() + "\n";
+                String data = Thread.currentThread().getName() + " value = " + counter.getCount() + "\n";
                 Files.write(Paths.get(PATH_FILE), data.getBytes(), StandardOpenOption.APPEND);
                 counter.increment();
+                logger.info(data);
             } catch (IOException e) {
                 throw new RuntimeException("Can't write in file by this path: " + PATH_FILE, e);
             }
